84a17bba203b5e9640597bc0fa0936d9adf3544b
Andy J. Wills <andy@willslab.co.uk>
2023-01-22 20:27:09 +0000

    Update init.el
    Tweaks towards anto-install and update of packages

--- init.el
+++ init.el
@@ -1,3 +1,40 @@
+(require 'cl)
+(require 'use-package)
+;; Package manager  
+(package-initialize)
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
+;; Guarantee all packages are installed on start
+(defvar packages-list
+  '(
+    auctex
+    elpy
+    markdown-mode
+    ess
+    polymode
+    poly-R
+    quarto-mode
+    yaml-mode
+    )
+  "List of packages needs to be installed at launch")
+
+(defun has-package-not-installed ()
+  (loop for p in packages-list
+        when (not (package-installed-p p)) do (return t)
+        finally (return nil)))
+
+(when (has-package-not-installed)
+  ;; Check for new packages (package versions)
+  (message "%s" "Get latest versions of all packages...")
+  (package-refresh-contents)
+  (message "%s" " done.")
+  ;; Install the missing packages
+  (dolist (p packages-list)
+    (when (not (package-installed-p p))
+      (package-install p))))
+
+
 ;; Auto-added
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -7,19 +44,13 @@
  '(ansi-color-names-vector
    ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
  '(column-number-mode t)
- '(custom-enabled-themes (quote (andy)))
+ '(custom-enabled-themes '(andy))
  '(custom-safe-themes
-   (quote
-    ("a16732011dcb5a3b50a5007d4c41b444761545e286ee58839931972c4455fe57" "84890723510d225c45aaff941a7e201606a48b973f0121cb9bcb0b9399be8cba" default)))
- '(org-agenda-files (quote ("~/svn/notes-andy/todo-new.org")))
- '(package-selected-packages
-   (quote
-    (quarto-mode yaml-mode ## poly-R polymode auctex fireplace notmuch ess elpy)))
+   '("a16732011dcb5a3b50a5007d4c41b444761545e286ee58839931972c4455fe57" "84890723510d225c45aaff941a7e201606a48b973f0121cb9bcb0b9399be8cba" default))
+ '(org-agenda-files '("~/svn/notes-andy/todo-new.org"))
+ '(package-selected-packages '(fireplace notmuch ess elpy))
  '(tool-bar-mode nil))
 
-;; I should install the following packages from MELPA
-;; M-x package-list-packages
-;; ess, auctex, polymode, poly-R, elpy, markdown-mode
 
 ;; Defaults across modes: GUI
 (tool-bar-mode -1) ;; No toolbar
@@ -28,11 +59,6 @@
 ;; Defaults across modes: Text editing
 (setq-default fill-column 79) ;; GNU standard width
 (setq column-number-mode t) ;; Show column number
-
-;; Package manager  
-(package-initialize)
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
 ;; Alphabetical list of modes starts below
 
@@ -54,9 +80,6 @@
      (define-key elpy-mode-map (kbd "<M-RET>") 'elpy-shell-send-statement-and-step)
      )
   )
-
-;; Also manually install these packages:
-;; pip install rope jedi importmagic autopep8 flake8
 
 ;; HTML mode
 (defun my-html-mode-hook ()
@@ -101,6 +124,9 @@
 (add-hook 'org-mode-hook 'turn-on-visual-line-mode)
 (setq org-startup-with-inline-images t)
 
+;; Quarto mode
+(require 'quarto-mode)
+
 ;; Text mode
 (add-hook 'text-mode-hook 'turn-on-visual-line-mode)
 
@@ -109,7 +135,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight normal :height 143 :width normal))))
+ '(default ((t (:inherit nil :stipple nil :background "#3F3F3F" :foreground "#DCDCCC" :inverse-video nil :box nil :strike-through nil :extend nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :foundry "nil" :family "Menlo"))))
  '(term-color-blue ((t (:background "blue2" :foreground "deep sky blue"))))
  '(term-color-red ((t (:background "red3" :foreground "tomato")))))
 
@@ -123,5 +149,3 @@
 ;; Region highlight colour
 (set-face-attribute 'region nil :background "#400000")
 
-;; load the library
-(require 'quarto-mode)
